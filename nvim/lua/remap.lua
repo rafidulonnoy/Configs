89b6a2022a0f2b807037b3649b372dfe7ad9bd76
Rafidul Onnoy <rafidulannoy@gmail.com>
2023-11-19 16:48:55 +0600

    Neovim configs added

---
+++ nvim/lua/remap.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.g.maplocalleader = " "
+vim.o.clipboard = "unnamedplus"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 4
+vim.o.updatetime = 300
+vim.o.termguicolors = true
+vim.o.mouse = "a"
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
